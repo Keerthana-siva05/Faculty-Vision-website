6c0af97a4e4026c26464a9f38b58ef4e6d980a08
Keerthana-siva05 <stkeerthana2003@gmail.com>
2025-03-12 10:46:33 +0530

    Delete routes directory

--- routes/facultyActivitiesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { updateFacultyActivities, getAllFacultyActivities } from "../controllers/facultyActivitiesController.js";
-
-const router = express.Router();
-
-router.put("/update-activities", updateFacultyActivities);
-router.get("/activities", getAllFacultyActivities);
-
-export default router;
