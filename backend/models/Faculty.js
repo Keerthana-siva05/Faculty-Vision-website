b5c6ecef161672385bee8baea5a33b1033ba9bfd
Keerthana-siva05 <stkeerthana2003@gmail.com>
2025-03-12 10:45:27 +0530

    Delete backend directory

--- backend/models/Faculty.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const FacultySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  activities: { type: [String], default: [] }, // Store activities as an array
-});
-
-export default mongoose.model("Faculty", FacultySchema);
--- backend/routes/facultyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllFaculty } from "../controllers/facultyController.js";
-
-const router = express.Router();
-
-router.get("/", getAllFaculty); // ✅ Make sure this route exists!
-
-export default router;
